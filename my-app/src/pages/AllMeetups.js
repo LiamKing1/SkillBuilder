2fe31508a8bc57591dc1f9af532b0010caca4a74
Liam King <techy.willyk3@gmail.com>
2023-10-20 13:30:06 -0500

    Ive got my GET function working properly pulling information from firebase and loading them to the dom.

--- my-app/src/pages/AllMeetups.js
+++ my-app/src/pages/AllMeetups.js
@@ -1,33 +1,47 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-// Data created only for testing purposes
-const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-
 function AllMeetups() {
+  const [loading, setLoading] = useState(true);
+  const [loadedMeets, setLoadedMeets] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      "https://react-skillbuilder-ba1e8-default-rtdb.firebaseio.com/mmetup.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meets = [];
+
+        for (const key in data) {
+          const meet = {
+            id: key,
+            ...data[key],
+          };
+          meets.push(meet);
+        }
+
+        setLoading(false);
+        setLoadedMeets(meets);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <section>
+        <p> Loading... </p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      {/* imported the MeetupList component and the prop that i used therin and use it to pass in the dummy data array's values*/}
-      <MeetupList meets={DUMMY_DATA} />
+      {/* imported the MeetupList component and the prop that i used therin and use it to pass in the data array's values*/}
+      <MeetupList meets={loadedMeets} />
       {/* Below was simply used for testing purposes */}
       {/* <ul>
         {DUMMY_DATA.map((meetup) => {
